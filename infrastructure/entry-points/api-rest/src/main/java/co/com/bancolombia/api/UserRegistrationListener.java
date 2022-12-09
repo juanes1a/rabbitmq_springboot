3311c73ce167dc2bc6b7eb3e828b274a5ec28e8f
Juan Esteban Gonzalez <juesgonz@bancolombia.com.co>
2022-12-09 10:04:05 -0500

    Retry mechanism for error handling

--- infrastructure/entry-points/api-rest/src/main/java/co/com/bancolombia/api/UserRegistrationListener.java
+++ infrastructure/entry-points/api-rest/src/main/java/co/com/bancolombia/api/UserRegistrationListener.java
@@ -1,14 +1,31 @@
 package co.com.bancolombia.api;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@AllArgsConstructor
 public class UserRegistrationListener {
+
+    @Autowired
+    public final RabbitTemplate rabbitTemplate;
+
     @RabbitListener(queues = {"q.user-registration"})
     public void onUserRegistration(UserRegistrationRequest event) {
         log.info("User Registration Event Received: {}", event);
+
+        executeRegistration(event);
+
+        rabbitTemplate.convertAndSend("x.post-registration", "", event);
+    }
+
+    private void executeRegistration(UserRegistrationRequest event) {
+        log.info("Executing User Registration Event: {}", event);
+        throw new RuntimeException("Registration Failed");
     }
 }
